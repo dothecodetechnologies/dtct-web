3240a113c3b9e979b05f473e72eea5e3c582c335
dothecodetechnologies <dothecode1@gmail.com>
2023-06-24 02:03:00 +0530

    image, databse connected, location jsonadded

---
+++ database/config.js
@@ -0,0 +1,8 @@
+//cred
+module.exports = {
+    HOST: 'sql12.freesqldatabase.com',
+    DB_USER: 'sql12627090',
+    DB_PASSWORD: 'las6sRHT6P',
+    DB: 'sql12627090',
+    PORT:3306
+}
---
+++ paytm/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    MID: 'vPvvYq64439671886544',
+    PAYTM_MERCHANT_KEY: '14mu7&ZtXSBLTZt8',
+    PAYTM_FINAL_URL: 'http://localhost:3001',
+    WEBSITE: 'WEBSTAGING',
+    CHANNEL_ID: 'WEB',
+    INDUSTRY_TYPE_ID: 'Retail',
+    CALLBACK_URL: 'http://localhost:3001/callback'
+  };
